06844dcbfb86fb58864ffea71d75bffe9365b660
Anesi-Daniella <anesi12@yahoo.com>
2019-10-16 16:15:42 +0100

    added a store index action

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
   def index
+    #Here Product is our old product model and order is a method that applies to the Product class and creates a list of all productsin the database
+    @products = Product.order(:title)
   end
 end
