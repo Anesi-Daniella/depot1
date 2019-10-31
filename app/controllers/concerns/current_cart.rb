750f8f8efcc5ff8322bc53fc633afae4b2af32d6
Anesi-Daniella <anesi12@yahoo.com>
2019-10-31 12:50:48 +0000

    added cart and line item models.

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -2,6 +2,7 @@ module CurrentCart
     private
   
     def set_cart
+      has_many :line_items, dependent: :destroy
       @cart = Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
       @cart = Cart.create
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,5 @@
 class LineItem < ApplicationRecord
+  # with the following any line item has 2 attributes .product and .cart which can be used to access the cart of a line item as li.cart
   belongs_to :product
   belongs_to :cart
 end
