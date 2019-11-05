be18c590ece18f0f6424c23d79e00f1bc28a9f25
Anesi-Daniella <anesi12@yahoo.com>
2019-11-05 14:58:13 +0000

    added quantity column to the line items table

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,15 +1,12 @@
 class Cart < ApplicationRecord
-    
-    module CurrentCart
-        private
 
-        # finds the user's cart or creates a new one if they dont already have one
-        def set_cart
-          @cart = Cart.find(session[:cart_id])
-        rescue ActiveRecord::RecordNotFound
-          @cart = Cart.create
-          session[:cart_id] = @cart.id
+    def add_product(product)
+        current_item = line_items.find_by(product_id: product.id)
+        if current_item
+          current_item.quantity += 1
+        else
+          current_item = line_items.build(product_id: product.id)
         end
+        current_item
       end
-
 end
