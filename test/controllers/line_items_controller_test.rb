3d6cdef2ea072292c591feb301a318a8916fec2c
Anesi-Daniella <anesi12@yahoo.com>
2019-11-18 00:29:28 +0000

    changes made in the line_items_controller_test.rb file

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,14 +17,10 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post line_items_url, params:  { product_id: products(:ruby).id }
+      post :create, product_id: products('1 x Programming Ruby 1.9 & 2.0')
     end
 
-    follow_redirect!
-
-    assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', '1 x Programming Ruby 1.9 & 2.0'
-    
+    assert_redirected_to cart_path(assigns(:line_item).cart)
   end
 
   test "should show line_item" do
